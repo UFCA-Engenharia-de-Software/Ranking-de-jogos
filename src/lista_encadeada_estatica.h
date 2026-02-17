26186f59cab3c16afb02ed34cdc9493149136df5
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2026-02-17 17:58:58 -0300

    feat: adiciona estrutura de lista encadeada estatica para armazenamento em vetor

--- src/lista_encadeada_estatica.h
+++ src/lista_encadeada_estatica.h
@@ -7,3 +7,23 @@ pré-alocado, e declara as funções responsáveis por sua manipulação.
 O gerenciamento manual das posições livres permite comparar esta
 estrutura com a lista encadeada dinâmica em termos de desempenho.
 */
+
+#ifndef LISTA_ENCADEADA_ESTATICA_H
+#define LISTA_ENCADEADA_ESTATICA_H
+
+#define MAX 100
+#define INVALIDO -1
+
+typedef struct {
+    int valor;
+    int proximo;
+    int anterior;
+} No;
+
+typedef struct {
+    No vetor[MAX];
+    int inicio;
+    int livre;
+} Lista;
+
+#endif
