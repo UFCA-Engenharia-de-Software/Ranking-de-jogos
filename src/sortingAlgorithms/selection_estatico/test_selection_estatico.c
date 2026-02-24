0601f74e7d6d7bcd4740f277a55fb2bbdc27f0dc
Salomão <salomao.silva@aluno.ufca.edu.br>
2026-02-24 09:08:18 -0300

    fix: Alteração do nome das funções nas chamadas para os implementados na lista encadeada estática.

--- src/sortingAlgorithms/selection_estatico/test_selection_estatico.c
+++ src/sortingAlgorithms/selection_estatico/test_selection_estatico.c
@@ -10,28 +10,28 @@ int main() {
     SetConsoleOutputCP(65001);
     Lista minha_lista;
     
-    initLista(&minha_lista); 
+    iniciarLista(&minha_lista); 
 
     Jogador j1 = {"Alice", 1500, 10, 50.5};
     Jogador j2 = {"Bob", 2000, 12, 60.0};
     Jogador j3 = {"Charlie", 1500, 15, 45.0};
     Jogador j4 = {"Diana", 3000, 20, 100.0};
 
-    insert(&minha_lista, j1);
-    insert(&minha_lista, j2);
-    insert(&minha_lista, j3);
-    insert(&minha_lista, j4);
+    inserir(&minha_lista, j1);
+    inserir(&minha_lista, j2);
+    inserir(&minha_lista, j3);
+    inserir(&minha_lista, j4);
 
     printf("--- LISTA ANTES DA ORDENACAO ---\n");
-    print(&minha_lista); 
+    imprimir(&minha_lista); 
 
     printf("\n--- ORDENANDO POR PONTUACAO ---\n");
     selection_sort_estatico(&minha_lista, ORDENAR_PONTUACAO);
-    print(&minha_lista);
+    imprimir(&minha_lista);
 
     printf("\n--- ORDENANDO POR CRITERIO COMBINADO ---\n");
     selection_sort_estatico(&minha_lista, ORDENAR_COMBINADO);
-    print(&minha_lista);
+    imprimir(&minha_lista);
 
     return 0;
 }
